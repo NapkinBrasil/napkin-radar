fc36d9585148d594dcaafa3663bf5a010241c3db
Rafael Pazini <rflpazini@gmail.com>
2016-08-11 17:05:14 -0300

    Add the project

---
+++ app/src/main/java/com/rflpazini/radar/RadarPoint.java
@@ -0,0 +1,25 @@
+package com.rflpazini.radar;
+
+/**
+ * Created by Karam Ahkouk on 04/06/15.
+ */
+public class RadarPoint {
+    double x;
+    double y;
+    int radius;
+
+    String identifier;
+
+    public RadarPoint(String identifier, double x, double y, int radius){
+        this.identifier = identifier;
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+    }
+
+    public RadarPoint(String identifier, double x, double y){
+        this.identifier = identifier;
+        this.x = x;
+        this.y = y;
+    }
+}
